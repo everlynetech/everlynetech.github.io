1c8b76c65e6ec1b887e1b602dca7e83df5f4477f
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2024-08-26 23:10:19 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -61,8 +61,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf469d72822c55d51ecb302d93e9f1c7
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c63292f21f8c44177a454d6b7c0b10eb",
-"/": "c63292f21f8c44177a454d6b7c0b10eb",
+"index.html": "01f8b14a540fd8dfe471c9f5d759a330",
+"/": "01f8b14a540fd8dfe471c9f5d759a330",
 "main.dart.js": "6c7ed25ed0773920cfb08acf5f74eb96",
 "manifest.json": "9685166c2173dd720e321bde3ae48f4b",
 "version.json": "ee68283cf4f4acb614f6fdf5512bdfab"};
